fa8f67e8d60769937b9efa05640bbc262842f538
SuperCoder <propscode254@gmail.com>
2023-10-02 12:54:58 +0200

    ad rounds handling

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+// 1. Save variables
+
+// 1.1 Rounds
+
+const roundWrapper = document.querySelector('[data-js="round-wrapper"]');
+const round3 = document.querySelector('[data-js="round3"]');
+const round5 = document.querySelector('[data-js="round5"]');
+const round7 = document.querySelector('[data-js="round7"]');
+const roundOutut = document.querySelector('[data-js="rounds-output"]');
+
+// 1.2 Play Options
+
+const rock = document.querySelector('[data-js="rock"]');
+const papper = document.querySelector('[data-js="rock"]');
+const scissors = document.querySelector('[data-js="rock"]');
+
+// 1.3 Outputs
+
+const userChoice = document.querySelector('[data-js="user-choice"]');
+const computerChoice = document.querySelector('[data-js="computer-choice"]');
+const userPoints = document.querySelector('[data-js="user-points"]');
+const computerPoints = document.querySelector('[data-js="computer-points"]');
+
+// 2. Functions startGame()  & restart()
+
+let rounds = 0;
+
+const startGame = () => {
+  // RESTART
+  roundOutut.style.display = "none";
+  roundOutut.innerHTML = "";
+
+  //  2.1 Save Work Values
+  let round3checked = round3.checked;
+  let round5checked = round5.checked;
+  let round7checked = round7.checked;
+  switch (true) {
+    case round3checked:
+      rounds = 3;
+      break;
+    case round5checked:
+      rounds = 5;
+      break;
+    case round7checked:
+      rounds = 7;
+      break;
+    default:
+      roundOutut.style.display = "block";
+      roundOutut.innerHTML =
+        "Please choose the number of rounds you want to play";
+      break;
+  }
+
+  console.log(rounds);
+};
