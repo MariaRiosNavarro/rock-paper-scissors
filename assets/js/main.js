59a58a18aa368e22bd9cd1b8e5aea444d698b767
SuperCoder <propscode254@gmail.com>
2023-10-02 13:52:06 +0200

    change

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,12 @@ const round3 = document.querySelector('[data-js="round3"]');
 const round5 = document.querySelector('[data-js="round5"]');
 const round7 = document.querySelector('[data-js="round7"]');
 const roundOutut = document.querySelector('[data-js="rounds-output"]');
+const roundsTotal = document.querySelector('[data-js="rounds-total"]');
+const roundsRest = document.querySelector('[data-js="rounds-rest"]');
 
 // 1.2 Play Options
 
+const playForm = document.querySelector('[data-js="play-choices"]');
 const rock = document.querySelector('[data-js="rock"]');
 const papper = document.querySelector('[data-js="rock"]');
 const scissors = document.querySelector('[data-js="rock"]');
@@ -22,17 +25,26 @@ const computerChoice = document.querySelector('[data-js="computer-choice"]');
 const userPoints = document.querySelector('[data-js="user-points"]');
 const computerPoints = document.querySelector('[data-js="computer-points"]');
 
-// 2. Functions startGame()  & restart()
+// 2. Functions roundForm, startGame()  & restart()
 
 let rounds = 0;
+let restRound;
 
-//  2.1 Save Work Values
+//  2.1 form / checked values
 
 roundForm.addEventListener("change", (event) => {
   event.preventDefault();
+  // restart
+  //   rounds = 0;
+  //   roundWrapper.style.display = "block";
+  //   roundOutut.style.display = "none";
+  //   roundOutut.innerHTML = "";
+
+  //   save values
   let round3checked = round3.checked;
   let round5checked = round5.checked;
   let round7checked = round7.checked;
+
   switch (true) {
     case round3checked:
       rounds = 3;
@@ -49,17 +61,16 @@ roundForm.addEventListener("change", (event) => {
         "Please choose the number of rounds you want to play";
       break;
   }
-});
-
-const startGame = () => {
-  // RESTART
-  roundOutut.style.display = "none";
-  roundOutut.innerHTML = "";
-  let roundContainer;
 
   // 2.2 After Choice display round Container counter
+  roundWrapper.style.display = "none";
+  roundsTotal.innerHTML = rounds;
+  console.log(rounds);
+  roundOutut.style.display = "block";
+});
 
-  roundContainer = document.createElement("span");
-  roundContainer.innerHTML = rounds;
-  roundOutut.innerHTML = roundContainer;
+const removeRound = () => {
+  rounds--;
+  restRound = rounds;
+  roundsRest.innerHTML = restRound;
 };
